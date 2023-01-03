b199955a366c1b7bf37d463e5cdc9a29d99f6896
AbinayaSittampalam <106548985+AbinayaSittampalam@users.noreply.github.com>
2023-01-03 14:38:47 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,8 +1,13 @@
 class sum{
+    {
+          public Static void main (String args[])
+        {
     int a=5;
     int b=3;
     int c=6;
     int d=7;
     int f;
     System.out.println(a+b); //sumation
+    System.out.println(a+b+c+D);
 }
+    }
